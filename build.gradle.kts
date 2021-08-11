a54bda19d1f13fea292299f283a7986d7268855c
xqs1995 <zyzz19951230@163.com>
2021-08-12 00:52:26 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,16 @@ plugins {
 group = "com.zimolab"
 version = "0.1.0-SNAPSHOT"
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
