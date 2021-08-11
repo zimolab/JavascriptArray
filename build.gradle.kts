ddb8cfbbdf758ee8ea29a12132ab9e65b6ad1915
xqs1995 <zyzz19951230@163.com>
2021-08-12 02:02:58 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,31 @@
 plugins {
     kotlin("jvm") version "1.5.10"
     java
+    `java-library`
     `maven-publish`
 }
 
-group = "com.zimolab"
+group = "com.github.zimolab"
 version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-}
-
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+subprojects {
+    apply {
+        plugin("org.gradle.java")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.gradle.maven-publish")
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib"))
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    }
 }
--- jsarray-core-library/build.gradle.kts
+++ jsarray-core-library/build.gradle.kts
@@ -1,21 +1,9 @@
 plugins {
-    kotlin("jvm")
-    java
     `java-library`
-    `maven-publish`
 }
-
-group = "com.zimolab"
-version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 java {
